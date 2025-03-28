668cd03879229cbbd4c957b593383fa7f330b203
“Cosette” <zaben2464@gmail.com>
2025-03-27 18:51:19 -0700

    ping pong

---
+++ pong/pong.js
@@ -0,0 +1,13 @@
+/canvas variables/
+canvas = document.getElementById("canvas");
+context = canvas.getContex("2d");
+/circle variables/
+var circle_diameter = 0
+var color_circle = "white";
+var position_circle = 0;
+/paddle variables/
+var width_paddle = 0
+var height_paddle = 0
+var color_paddle = "white";
+var color_paddle = 0;
+var position_paddle = 0;
