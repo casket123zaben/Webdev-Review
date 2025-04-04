7bd94080b841c03b14d3575d72ecdb08a032915a
“Cosette” <zaben2464@gmail.com>
2025-04-03 18:05:40 -0700

    paddle and ball creation

--- pong/pong.js
+++ pong/pong.js
@@ -11,3 +11,26 @@ var height_paddle = 0
 var color_paddle = "white";
 var color_paddle = 0;
 var position_paddle = 0;
+/paddle creation function/
+function paddle(){
+    context.startPath();
+    ctx.lineWidth = 4;
+    ctx.moveTo(0, -5);
+    ctx.lineTo(-10, 15);
+    ctx.lineTo(10, 15);
+    ctx.closePath();
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+    ctx.fillStyle = "white";
+    ctx.fill();
+}
+/circle creation function/
+function circle(){
+    context.arc(30, 16, 10, 0, 2*Math.PI);
+    context.strokeStyle = "white";
+    context.lineWidth = 10;
+    context.stroke();
+    context.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
