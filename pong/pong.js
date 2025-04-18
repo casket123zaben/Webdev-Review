4ecd620f232201db4d5d03e946e2755b1e826e1b
“Cosette” <zaben2464@gmail.com>
2025-04-17 18:46:55 -0700

    4/17/25

--- pong/pong.js
+++ pong/pong.js
@@ -1,40 +1,59 @@
 //canvas variables//
 cv = document.getElementById("canvas");
-ctx = canvas.getContex("2d");
-cvs.width = 800;
+ctx = canvas.getContext("2d");
+cv.width = 800;
 cv.height = 500;
+//speed of the ball(velocity)//
+var x_vel = 2;
+var y_vel = 2;
 //ball variables//
-var ball_size = 10; //(diameter)//
+var ball_size = 20; //(diameter)//
 var ball_color = "white";
 var ball_x = 400;
 var ball_y = 250;
 //paddle variables//
-var width_paddle = 20;
-var height_paddle = 80;
+var width_paddle = 30;
+var height_paddle = 120;
 var paddle_color = "white";
 var color_paddle = 0;
-var position_paddle_x_left = 100;
-var position_paddle_y_left = 250;
-var position_paddle_x_right = 700;
-var position_paddle_y_right = 250;
-//calling paddle function//
-createPaddle(width_paddle, height_paddle, color_paddle, paddle_x_left, paddle_y_left);
+var position_paddle_x_left = 95;
+var position_paddle_y_left = 210;
+var position_paddle_x_right = 675;
+var position_paddle_y_right = 210;
+//calling left paddle function//
+createLeftPaddle(width_paddle, height_paddle, paddle_color, position_paddle_x_left, position_paddle_y_left);
+//calling right paddle function//
+createRightPaddle(width_paddle, height_paddle, paddle_color, position_paddle_x_right, position_paddle_y_right);
 //paddle creation function//
-function createPaddle(w,h,c,x,y){
+//left paddle//
+function createLeftPaddle(w,h,c,x,y){
     ctx.beginPath();
     ctx.rect(x,y,w,h);
     ctx.closePath();
     ctx.fillStyle = c;
     ctx.fill();
 }
-
-//circle creation function//
-function circle(){
-    context.arc(300, 16, 10, 0, 2*Math.PI);
-    context.strokeStyle = "white";
-    context.lineWidth = 10;
-    context.stroke();
-    context.fillStyle = "white";
-    ctx.fill();
+//right paddle//
+function createRightPaddle(w,h,c,x,y){
+    ctx.beginPath();
+    ctx.rect(x,y,w,h);
     ctx.closePath();
+    ctx.fillSyle = c;
+    ctx.fill();
+}
+//calling ball function//
+createBall(ball_x, ball_y, ball_size,ball_color);
+//ball creation funciton//
+function createBall(x,y,r,c){
+    var path = new Path2D()
+    path.arc(x,y,r,0,2 * Math.PI);
+    ctx.fillStyle = c;
+    ctx.fill(path)
+}
+//function for moving the ball//
+function moveBall(){
+    //definging left bounds//
+    if(ball_x + x_vel > cv.width){
+
+    }
 }
